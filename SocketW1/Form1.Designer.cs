0e679bd143b97b6dcc8cee9738093bb77a675b77
dingxu6207 <32772701+dingxu6207@users.noreply.github.com>
2018-04-29 19:30:25 +0800

    c#测窗控制

---
+++ SocketW1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SocketW1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.port = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBeginListen = new System.Windows.Forms.Button();
+            this.lbOnline = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 12);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(343, 118);
+            this.txtMsg.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP:";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(419, 25);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(100, 21);
+            this.txtIp.TabIndex = 2;
+            // 
+            // port
+            // 
+            this.port.AutoSize = true;
+            this.port.Location = new System.Drawing.Point(380, 55);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(35, 12);
+            this.port.TabIndex = 3;
+            this.port.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(419, 52);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 21);
+            this.txtPort.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "在线列表";
+            // 
+            // btnBeginListen
+            // 
+            this.btnBeginListen.Location = new System.Drawing.Point(384, 239);
+            this.btnBeginListen.Name = "btnBeginListen";
+            this.btnBeginListen.Size = new System.Drawing.Size(135, 23);
+            this.btnBeginListen.TabIndex = 7;
+            this.btnBeginListen.Text = "启动服务";
+            this.btnBeginListen.UseVisualStyleBackColor = true;
+            this.btnBeginListen.Click += new System.EventHandler(this.btnBeginListen_Click);
+            // 
+            // lbOnline
+            // 
+            this.lbOnline.FormattingEnabled = true;
+            this.lbOnline.ItemHeight = 12;
+            this.lbOnline.Location = new System.Drawing.Point(384, 97);
+            this.lbOnline.Name = "lbOnline";
+            this.lbOnline.Size = new System.Drawing.Size(158, 124);
+            this.lbOnline.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 275);
+            this.Controls.Add(this.lbOnline);
+            this.Controls.Add(this.btnBeginListen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMsg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label port;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBeginListen;
+        private System.Windows.Forms.ListBox lbOnline;
+    }
+}
+
---
+++ SocketW1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.Net;  // IP，IPAddress, IPEndPoint，端口等；
+using System.Threading;
+using System.IO;
+
+namespace SocketW1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            GetIp();
+        }
+
+        void  GetIp()
+        {
+            //获取本地的IP地址
+            string AddressIP = string.Empty;
+            foreach (IPAddress _IPAddress in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (_IPAddress.AddressFamily.ToString() == "InterNetwork")
+                {
+                    AddressIP = _IPAddress.ToString();
+                }
+            }
+            //给IP控件赋值
+            txtIp.Text = AddressIP;       
+        }
+
+
+        Thread threadWatch = null; // 负责监听客户端连接请求的 线程；
+        Socket socketWatch = null;
+
+        Dictionary<string, Socket> dict = new Dictionary<string, Socket>();
+        Dictionary<string, Thread> dictThread = new Dictionary<string, Thread>();
+
+        private void btnBeginListen_Click(object sender, EventArgs e)
+        {
+            // 创建负责监听的套接字，注意其中的参数；
+            socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // 获得文本框中的IP对象；
+            IPAddress address = IPAddress.Parse(txtIp.Text.Trim());
+            // 创建包含ip和端口号的网络节点对象；
+            IPEndPoint endPoint = new IPEndPoint(address, int.Parse(txtPort.Text.Trim()));
+
+            try
+            {
+                // 将负责监听的套接字绑定到唯一的ip和端口上；
+                socketWatch.Bind(endPoint);
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show("异常：" + se.Message);
+                return;
+            }
+
+            // 设置监听队列的长度；
+            socketWatch.Listen(10);
+            // 创建负责监听的线程；
+            threadWatch = new Thread(WatchConnecting);
+            threadWatch.IsBackground = true;
+            threadWatch.Start();
+            ShowMsg("服务器启动监听成功！");
+
+        }
+
+        void ShowMsg(string str)
+        {
+            txtMsg.AppendText(str + "\r\n");
+        }
+
+        /// <summary>
+        /// 监听客户端请求的方法；
+        /// </summary>
+        void WatchConnecting()
+        {
+            while (true)  // 持续不断的监听客户端的连接请求；
+            {
+                // 开始监听客户端连接请求，Accept方法会阻断当前的线程；
+                Socket sokConnection = socketWatch.Accept(); // 一旦监听到一个客户端的请求，就返回一个与该客户端通信的 套接字；
+                // 想列表控件中添加客户端的IP信息；
+                lbOnline.Items.Add(sokConnection.RemoteEndPoint.ToString());
+                // 将与客户端连接的 套接字 对象添加到集合中；
+                dict.Add(sokConnection.RemoteEndPoint.ToString(), sokConnection);
+                ShowMsg("客户端连接成功！");
+                Thread thr = new Thread(RecMsg);
+                thr.IsBackground = true;
+                thr.Start(sokConnection);
+                dictThread.Add(sokConnection.RemoteEndPoint.ToString(), thr);  //  将新建的线程 添加 到线程的集合中去。
+            }
+        }
+
+        void RecMsg(object sokConnectionparn)
+        {
+            Socket sokClient = sokConnectionparn as Socket;
+            while (true)
+            {
+                // 定义一个2M的缓存区；
+                byte[] arrMsgRec = new byte[1024 * 1024 * 2];
+                // 将接受到的数据存入到输入  arrMsgRec中；
+                int length = -1;
+                try
+                {
+                    length = sokClient.Receive(arrMsgRec); // 接收数据，并返回数据的长度；
+                }
+                catch (SocketException se)
+                {
+                    ShowMsg("异常：" + se.Message);
+                    // 从 通信套接字 集合中删除被中断连接的通信套接字；
+                    dict.Remove(sokClient.RemoteEndPoint.ToString());
+                    // 从通信线程集合中删除被中断连接的通信线程对象；
+                    dictThread.Remove(sokClient.RemoteEndPoint.ToString());
+                    // 从列表中移除被中断的连接IP
+                    lbOnline.Items.Remove(sokClient.RemoteEndPoint.ToString());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    ShowMsg("异常：" + e.Message);
+                    // 从 通信套接字 集合中删除被中断连接的通信套接字；
+                    dict.Remove(sokClient.RemoteEndPoint.ToString());
+                    // 从通信线程集合中删除被中断连接的通信线程对象；
+                    dictThread.Remove(sokClient.RemoteEndPoint.ToString());
+                    // 从列表中移除被中断的连接IP
+                    lbOnline.Items.Remove(sokClient.RemoteEndPoint.ToString());
+                    break;
+                }
+                if (arrMsgRec[0] == 0)  // 表示接收到的是数据；
+                {
+                    string strMsg = System.Text.Encoding.UTF8.GetString(arrMsgRec, 1, length - 1);// 将接受到的字节数据转化成字符串；
+                    ShowMsg(strMsg);
+                }
+               
+            }
+        }
+
+
+    }
+}
